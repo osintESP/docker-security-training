e37eaa9ae7bc9a6cfe1f435a11531a6792dffac9
metaverse0022 <metaverse0022@gmail.com>
2025-11-21 23:26:29 -0300

    Completados Ejercicios 4 y 5: Hardening de Imagen (v2.0, Multi-Stage, SHA256) y Hardening de Runtime (cap_drop ALL)

--- docker-security-training/ejercicio-2-api/api.py
+++ docker-security-training/ejercicio-2-api/api.py
@@ -1,10 +1,11 @@
 from flask import Flask
+import socket # Importamos socket
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return '<h1>Hola Seguro, desde Python!</h1>'
+    hostname = socket.gethostname() # Obtenemos el nombre del contenedor
+    return f'<h1>Hola Seguro, desde {hostname}!</h1>' # Devolvemos el nombre
 
 if __name__ == '__main__':
-    # Flask por defecto escucha en 0.0.0.0:5000
     app.run(host='0.0.0.0', port=5000)
